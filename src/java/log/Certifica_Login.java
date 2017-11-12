27b3d62afda376e6cb860cb09933a3477cbd5631
Carlos Henrique <carlos_hdc@hotmail.com>
2017-11-12 15:14:57 -0200

    filtro add

---
+++ src/java/log/Certifica_Login.java
@@ -0,0 +1,56 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package log;
+
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebFilter;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
+
+@WebFilter(filterName = "Certifica_Login", urlPatterns = {"/Tabelas","/selecionar.jsp"})
+public class Certifica_Login implements Filter {
+    
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response,FilterChain chain) throws IOException, ServletException {
+        HttpSession sessao = ((HttpServletRequest) request).getSession();
+        Object logado = sessao.getAttribute("logado");
+        if (logado != null) {
+            String aux = (String) logado;
+            if (logado.equals("true")) {
+                chain.doFilter(request, response);
+            } else {
+                RequestDispatcher rd = request.getRequestDispatcher("areaBloqueada.jsp");
+                rd.forward(request, response);
+            }
+        } else {
+            RequestDispatcher rd = request.getRequestDispatcher("areaBloqueada.jsp");
+            rd.forward(request, response);
+        
+        }
+        
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) throws ServletException {
+    
+    }
+
+    @Override
+    public void destroy() {
+    
+    }
+}
