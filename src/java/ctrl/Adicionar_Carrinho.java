50fd1b1adbb0305d1240741d430efe5e4b006191
Carlos Henrique <carlos_hdc@hotmail.com>
2017-12-07 16:23:00 -0200

    inserir e mostrar cookies adicionados

---
+++ src/java/ctrl/Adicionar_Carrinho.java
@@ -0,0 +1,27 @@
+package ctrl;
+import DAO.Produto;
+import DAO.Produto_DAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpCookie;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Adicionar_Carrinho", urlPatterns = {"/Adicionar_Carrinho"})
+public class Adicionar_Carrinho extends HttpServlet{
+        
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String id = request.getParameter(""),nome = request.getParameter("");
+        if ((id == null) || (nome==null) || (id.equals("")) || (id.equals(""))){
+            request.getRequestDispatcher("carrinho.jsp").forward(request,response);
+        }
+        Cookie c = new Cookie(id,nome);
+        response.addCookie(c);
+        request.getRequestDispatcher("carrinho.jsp").forward(request,response);
+    }
+}
---
+++ src/java/ctrl/Retorna_Produtos.java
@@ -0,0 +1,36 @@
+package ctrl;
+import DAO.Produto;
+import DAO.Produto_DAO;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.net.HttpCookie;
+import java.util.ArrayList;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet(name = "Retorna_Produtos", urlPatterns = {"/Retorna_Produtos"})
+public class Retorna_Produtos extends HttpServlet{
+    
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        Cookie[] list = request.getCookies();
+        Cookie c;
+        List<String> resp = new ArrayList<>();
+        if(list!=null){
+            for (int i = 0; i < list.length; i++) {
+               c = list[i];
+               Produto p = (new Produto_DAO()).busca_produto((new Produto(Integer.parseInt(c.getName()))));
+               String aux = Integer.toString(p.getId_categoria())+"¨";
+               aux = aux + p.getNome()+"¨"+p.getDescricao()+"¨"+Float.toString(p.getPreco());
+               resp.add(aux);
+            }
+        }
+        request.setAttribute("r",resp);
+        request.getRequestDispatcher("carrinho.jsp").forward(request,response);
+    }
+}
